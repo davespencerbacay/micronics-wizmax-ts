7610ad58b277d0d5d958ce3ca8539ca452b29b7b
marklouie <marklouieluna31@gmail.com>
2022-10-27 12:51:37 -0700

    adding shoppage text

--- Web/src/pages/Shop/Shop.tsx
+++ Web/src/pages/Shop/Shop.tsx
@@ -1,11 +1,137 @@
 import PageTitle from "library/PageTitle/PageTitle";
 import React from "react";
+import Banner from "library/Banner/Banner";
+import { Col, Container, Row } from "reactstrap";
 
 const Shop: React.FC = () => {
   return (
     <React.Fragment>
-      <PageTitle title="Products" />
-      <p>Products</p>
+      <Banner
+        title="Find Your Local Store"
+        variant="light"
+        align="center"
+        subTitle="Get the best products for your workspace "
+      />
+
+      <Container>
+        <div className="shop-heading-tag">
+          <h5>Africa, Middle East, and India</h5>
+          <Row>
+            <Col xs={12} md={6} lg={4}>
+              <ul>
+                <li>Bahrain</li>
+                <li>Botswana</li>
+                <li>Cameroun</li>
+                <li>Nigeria</li>
+                <li>Egypt</li>
+                <li>Guinea-Bissau</li>
+                <li>Gunee Equatoriale</li>
+                <li>Niger</li>
+              </ul>
+            </Col>
+            <Col xs={12} md={6} lg={4}>
+              <ul>
+                <li>Bahrain</li>
+                <li>Botswana</li>
+                <li>Cameroun</li>
+                <li>Nigeria</li>
+                <li>Egypt</li>
+                <li>Guinea-Bissau</li>
+                <li>Gunee Equatoriale</li>
+                <li>Niger</li>
+              </ul>
+            </Col>
+            <Col xs={12} md={6} lg={4}>
+              <ul>
+                <li>Bahrain</li>
+                <li>Botswana</li>
+                <li>Cameroun</li>
+                <li>Nigeria</li>
+                <li>Egypt</li>
+                <li>Guinea-Bissau</li>
+                <li>Gunee Equatoriale</li>
+                <li>Niger</li>
+              </ul>
+            </Col>
+          </Row>
+        </div>
+        <Row>
+          <Col xs={12} md={6} lg={4}>
+            <ul>
+              <li>Bahrain</li>
+              <li>Botswana</li>
+              <li>Cameroun</li>
+              <li>Nigeria</li>
+              <li>Egypt</li>
+              <li>Guinea-Bissau</li>
+              <li>Gunee Equatoriale</li>
+              <li>Niger</li>
+            </ul>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ul>
+              <li>Bahrain</li>
+              <li>Botswana</li>
+              <li>Cameroun</li>
+              <li>Nigeria</li>
+              <li>Egypt</li>
+              <li>Guinea-Bissau</li>
+              <li>Gunee Equatoriale</li>
+              <li>Niger</li>
+            </ul>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ul>
+              <li>Bahrain</li>
+              <li>Botswana</li>
+              <li>Cameroun</li>
+              <li>Nigeria</li>
+              <li>Egypt</li>
+              <li>Guinea-Bissau</li>
+              <li>Gunee Equatoriale</li>
+              <li>Niger</li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6} lg={4}>
+            <ul>
+              <li>Bahrain</li>
+              <li>Botswana</li>
+              <li>Cameroun</li>
+              <li>Nigeria</li>
+              <li>Egypt</li>
+              <li>Guinea-Bissau</li>
+              <li>Gunee Equatoriale</li>
+              <li>Niger</li>
+            </ul>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ul>
+              <li>Bahrain</li>
+              <li>Botswana</li>
+              <li>Cameroun</li>
+              <li>Nigeria</li>
+              <li>Egypt</li>
+              <li>Guinea-Bissau</li>
+              <li>Gunee Equatoriale</li>
+              <li>Niger</li>
+            </ul>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ul>
+              <li>Bahrain</li>
+              <li>Botswana</li>
+              <li>Cameroun</li>
+              <li>Nigeria</li>
+              <li>Egypt</li>
+              <li>Guinea-Bissau</li>
+              <li>Gunee Equatoriale</li>
+              <li>Niger</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 };
