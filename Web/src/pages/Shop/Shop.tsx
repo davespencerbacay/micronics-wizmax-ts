5e01f17a328185a50a2bede2e76ba638dfa01e8a
marklouie <marklouieluna31@gmail.com>
2022-11-15 13:55:47 -0800

    additional for display dropdown design

--- Web/src/pages/Shop/Shop.tsx
+++ Web/src/pages/Shop/Shop.tsx
@@ -1,12 +1,70 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Banner from "library/Banner/Banner";
 import { Col, Container, Row } from "reactstrap";
 import "./Shop.scss";
 import { ThemeContext } from "context/ThemeContext";
+import { Link } from "react-router-dom";
+import {
+  ecuadorShop,
+  japanShop,
+  koreaShop,
+  philippinesShop,
+  usaShop,
+} from "./ShopLinks";
 
 const Shop: React.FC = () => {
   const themeCtx = useContext(ThemeContext);
   const isDarkMode = themeCtx.state.darkMode;
+
+  const [japanLink, setJapanLink] = useState(false);
+  const [koreaLink, setKoreaLink] = useState(false);
+  const [philippinesLink, setPhilippinesLink] = useState(false);
+  const [usaLink, setUsaLink] = useState(false);
+  const [ecuadorLink, setEcuadorLink] = useState(false);
+
+  const koreaLinkHandler = () => {
+    setKoreaLink((prevState) => !prevState);
+    setPhilippinesLink(false);
+    setUsaLink(false);
+    setEcuadorLink(false);
+    setJapanLink(false);
+  };
+  const philippinesHandler = () => {
+    setPhilippinesLink((prevState) => !prevState);
+    setKoreaLink(false);
+    setUsaLink(false);
+    setEcuadorLink(false);
+    setJapanLink(false);
+  };
+  const usaHandler = () => {
+    setUsaLink((prevState) => !prevState);
+    setKoreaLink(false);
+    setPhilippinesLink(false);
+    setEcuadorLink(false);
+    setJapanLink(false);
+  };
+  const ecuadorHandler = () => {
+    setEcuadorLink((prevState) => !prevState);
+    setKoreaLink(false);
+    setPhilippinesLink(false);
+    setUsaLink(false);
+    setJapanLink(false);
+  };
+  const japanLinkHandler = () => {
+    setJapanLink((prevState) => !prevState);
+    setKoreaLink(false);
+    setPhilippinesLink(false);
+    setUsaLink(false);
+    setEcuadorLink(false);
+  };
+
+  const handleCloseDropdown = () => {
+    setKoreaLink(false);
+    setUsaLink(false);
+    setEcuadorLink(false);
+    setPhilippinesLink(false);
+    setJapanLink(false);
+  };
 
   return (
     <React.Fragment>
@@ -67,16 +125,95 @@ const Shop: React.FC = () => {
             </Col>
             <Col xs={12} md={6} lg={4}>
               <ul>
-                <li>Japan(日本)</li>
+                <div className="dropdown" onMouseLeave={handleCloseDropdown}>
+                  <Link to="#" onClick={japanLinkHandler}>
+                    Japan(日本)
+                    <i className="fa-solid fa-angle-down"></i>
+                  </Link>
+                  <div
+                    className={
+                      japanLink ? "dropdown-content" : "dropdown-content-hide"
+                    }
+                    style={{
+                      visibility: japanLink ? "visible" : "hidden",
+                      opacity: japanLink ? "1" : "0",
+                    }}
+                  >
+                    {japanShop.map((shop) => {
+                      return (
+                        <a
+                          href={shop.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {shop.name}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
                 <li>Macao(澳門)</li>
-                <li>Korea(대한민국)</li>
+                <div className="dropdown" onMouseLeave={handleCloseDropdown}>
+                  <Link to="#" onClick={koreaLinkHandler}>
+                    Korea(대한민국)
+                    <i className="fa-solid fa-angle-down"></i>
+                  </Link>
+                  <div
+                    className={
+                      koreaLink ? "dropdown-content" : "dropdown-content-hide"
+                    }
+                    style={{
+                      visibility: koreaLink ? "visible" : "hidden",
+                      opacity: koreaLink ? "1" : "0",
+                    }}
+                  >
+                    {koreaShop.map((shop) => {
+                      return (
+                        <a
+                          href={shop.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {shop.name}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
                 <li>Malaysia</li>
                 <li>New Zealand</li>
               </ul>
             </Col>
             <Col xs={12} md={6} lg={4}>
               <ul>
-                <li>Philippines </li>
+                <div className="dropdown" onMouseLeave={handleCloseDropdown}>
+                  <Link to="#" onClick={philippinesHandler}>
+                    Philippines <i className="fa-solid fa-angle-down"></i>
+                  </Link>
+                  <div
+                    className={
+                      philippinesLink
+                        ? "dropdown-content"
+                        : "dropdown-content-hide"
+                    }
+                    style={{
+                      visibility: philippinesLink ? "visible" : "hidden",
+                      opacity: philippinesLink ? "1" : "0",
+                    }}
+                  >
+                    {philippinesShop.map((shop) => {
+                      return (
+                        <a
+                          href={shop.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {shop.name}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
                 <li>Singapore</li>
                 <li>Taiwan(台灣)</li>
                 <li>Thailand(ไทย)</li>
@@ -118,10 +255,60 @@ const Shop: React.FC = () => {
             <h5>North and South America</h5>
             <Col xs={12} md={6} lg={4}>
               <ul>
-                <li>United States</li>
+                <div className="dropdown" onMouseLeave={handleCloseDropdown}>
+                  <Link to="#" onClick={usaHandler}>
+                    United States <i className="fa-solid fa-angle-down"></i>
+                  </Link>
+                  <div
+                    className={
+                      usaLink ? "dropdown-content" : "dropdown-content-hide"
+                    }
+                    style={{
+                      visibility: usaLink ? "visible" : "hidden",
+                      opacity: usaLink ? "1" : "0",
+                    }}
+                  >
+                    {usaShop.map((shop) => {
+                      return (
+                        <a
+                          href={shop.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {shop.name}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
                 <li>Canada (English)</li>
                 <li>Canada (Français)</li>
-                <li>Ecuador </li>
+                <div className="dropdown" onMouseLeave={handleCloseDropdown}>
+                  <Link to="#" onClick={ecuadorHandler}>
+                    Ecuador <i className="fa-solid fa-angle-down"></i>
+                  </Link>
+                  <div
+                    className={
+                      ecuadorLink ? "dropdown-content" : "dropdown-content-hide"
+                    }
+                    style={{
+                      visibility: ecuadorLink ? "visible" : "hidden",
+                      opacity: ecuadorLink ? "1" : "0",
+                    }}
+                  >
+                    {ecuadorShop.map((shop) => {
+                      return (
+                        <a
+                          href={shop.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {shop.name}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
                 <li>Puerto Rico (Español)</li>
               </ul>
             </Col>
