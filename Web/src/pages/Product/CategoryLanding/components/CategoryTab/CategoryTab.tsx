a804f6ea9b01bb602f60c1fd761ebf176e214d5e
ruzzle <ruzzlebuhain.micronics@gmail.com>
2022-12-27 18:07:02 +0800

    fix navbar in product categories

--- Web/src/pages/Product/CategoryLanding/components/CategoryTab/CategoryTab.tsx
+++ Web/src/pages/Product/CategoryLanding/components/CategoryTab/CategoryTab.tsx
@@ -1,8 +1,9 @@
 import { ROUTE_PATH } from "constants/routes";
 import { productCategories } from "data/productCategories";
+import useResponsive from "hooks/useResponsive";
 import intl from "i18n/intl";
 import GoTo from "library/Images/Navigations/GoTo/GoTo";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Nav, NavItem } from "reactstrap";
 import "./CategoryTab.scss";
@@ -12,26 +13,53 @@ type CategoryTabType = {
 };
 
 const CategoryTab: React.FC<CategoryTabType> = (props) => {
+  const isMobile = useResponsive("mobile");
+  const [navClick, isNavClick] = useState(false);
+
+  const navClickHandler = () => {
+    isNavClick((prevState) => !prevState);
+  };
   return (
-    <div>
-      <Nav className="nav-container" pills>
-        {productCategories.map((cat, index) => (
-          <NavItem className="nav-items" key={cat.categoryId}>
-            <p onClick={() => props.refLinkHandler(index)}>{cat.name}</p>
-            {/* <Link to={cat.link}>{cat.name}</Link> */}
-          </NavItem>
-        ))}
-      </Nav>
-      <div className="shop-now">
-        {intl("productPage.checkAndShop")} &nbsp;
-        <Link to={ROUTE_PATH.SHOP}>
-          <b>
-            {intl("productPage.shopNow")}
-            <GoTo width={10}></GoTo>
-          </b>
-        </Link>
-      </div>
-    </div>
+    <React.Fragment>
+      {isMobile ? (
+        <div className="nav">
+          <Nav className="nav-container" pills>
+            {productCategories.map((cat, index) => (
+              <NavItem className="nav-items" key={cat.categoryId}>
+                {navClick && (
+                  <p onClick={() => props.refLinkHandler(index)}>{cat.name}</p>
+                )}
+
+                {/* <Link to={cat.link}>{cat.name}</Link> */}
+              </NavItem>
+            ))}
+            <div className="arrow-container" onClick={navClickHandler}>
+              <span></span>
+            </div>
+          </Nav>
+        </div>
+      ) : (
+        <div>
+          <Nav className="nav-container" pills>
+            {productCategories.map((cat, index) => (
+              <NavItem className="nav-items" key={cat.categoryId}>
+                <p onClick={() => props.refLinkHandler(index)}>{cat.name}</p>
+                {/* <Link to={cat.link}>{cat.name}</Link> */}
+              </NavItem>
+            ))}
+          </Nav>
+          <div className="shop-now">
+            {intl("productPage.checkAndShop")} &nbsp;
+            <Link to={ROUTE_PATH.SHOP}>
+              <b>
+                {intl("productPage.shopNow")}
+                <GoTo width={10}></GoTo>
+              </b>
+            </Link>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
