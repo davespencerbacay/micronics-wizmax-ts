47d9a4ef69248e3a9a8fae397a62deb4a997139e
Jonathan RB <jonathanrb@lingjack.com>
2023-05-21 21:55:23 +0800

    created link to learn more button in main view page

--- Web/src/pages/Home/HomeLanding/Sections/MainSection/CarouselViews/MainView.tsx
+++ Web/src/pages/Home/HomeLanding/Sections/MainSection/CarouselViews/MainView.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { HOME_IMAGES } from "constants/home";
+import { useNavigate } from "react-router-dom";
 
 const MainView: React.FC = () => {
+  const navigate = useNavigate();
+  const goTo = () => {
+    navigate("/products/CATEGORY_HEADSET/W301");
+  };
   return (
     <div className="main">
       <div className="details-container">
@@ -13,7 +18,9 @@ const MainView: React.FC = () => {
       <div className="img-container">
         <img src={HOME_IMAGES.HOME_HEADSET_SECTION.LISTEN_HEADSET} alt="W301" />
       </div>
-      <button className="learn-more-btn">Learn More</button>
+      <button className="learn-more-btn" onClick={goTo}>
+        Learn More
+      </button>
     </div>
   );
 };
