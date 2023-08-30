0ab837bc81c311e72db7c31058415e21dd2dcebc
Darryle <dmilesbacay@gmail.com>
2023-08-30 17:10:39 +0800

    create avatar setter helper

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -6,144 +6,146 @@ import generateToken from "../utils/generateToken.js";
 // route  POST/api/users/login
 // access Private
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+	const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.firstName + " " + user.lastName,
-      email: user.email,
-      token: generateToken(res, user._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid Username or password!");
-  }
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			name: user.firstName + " " + user.lastName,
+			email: user.email,
+			token: generateToken(res, user._id),
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid Username or password!");
+	}
 });
 
 const logOutUser = asyncHandler(async (req, res) => {
-  res.cookie("jwt", "", {
-    httpOnly: true,
-    expires: new Date(0),
-  });
-  res.status(200).json({ message: "Logged out successfully." });
+	res.cookie("jwt", "", {
+		httpOnly: true,
+		expires: new Date(0),
+	});
+	res.status(200).json({ message: "Logged out successfully." });
 });
 
 // @desc  Fetch All Users
 // route  GET/api/users
 // access Private
 const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
-  res.status(200).json(users);
+	const users = await User.find({});
+	res.status(200).json(users);
 });
 
 // @desc  Fetch Single User
 // route  GET/api/users/:id
 // access Private
 const getUserById = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
-  if (user) {
-    res.status(200).json(user);
-  } else {
-    res.status(404).json("[User] not Found!");
-  }
+	const user = await User.findById(req.params.id);
+	if (user) {
+		res.status(200).json(user);
+	} else {
+		res.status(404).json("[User] not Found!");
+	}
 });
 
 // @desc  Create Single User
 // route  POST/api/users/
 // access Private
 const createUser = asyncHandler(async (req, res) => {
-  const { email, password, firstName, lastName, isAdmin } = req.body;
+	const { email, password, firstName, lastName, isAdmin } = req.body;
 
-  const userExist = await User.findOne({ email });
-  if (userExist) {
-    res.status(400);
-    throw new Error("User already exist");
-  }
-  const user = await User.create({
-    firstName,
-    lastName,
-    email,
-    password,
-    isAdmin,
-  });
+	const userExist = await User.findOne({ email });
+	if (userExist) {
+		res.status(400);
+		throw new Error("User already exist");
+	}
+	const user = await User.create({
+		firstName,
+		lastName,
+		email,
+		password,
+		isAdmin,
+	});
 
-  if (user) {
-    generateToken(res, user._id);
+	if (user) {
+		generateToken(res, user._id);
 
-    res.status(201).json({
-      _id: user._id,
-      name: user.firstName + " " + user.lastName,
-      email: user.email,
-      password: user.password,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(400);
-    throw new Error("Invalid User Data!");
-  }
+		res.status(201).json({
+			_id: user._id,
+			name: user.firstName + " " + user.lastName,
+			email: user.email,
+			password: user.password,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid User Data!");
+	}
 });
 
 // @desc  Update User
 // route  PUT/api/users/
 // access Private
 const updateUserById = asyncHandler(async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
+	try {
+		const user = await User.findById(req.user._id);
 
-    if (user) {
-      user.firstName = req.body.firstName || user.firstName;
-      user.lastName = req.body.lastName || user.lastName;
-      user.email = req.body.email || user.email;
+		if (user) {
+			user.firstName = req.body.firstName || user.firstName;
+			user.lastName = req.body.lastName || user.lastName;
+			user.email = req.body.email || user.email;
 
-      if (req.body.password) {
-        user.password = req.body.password;
-      }
+			if (req.body.password) {
+				user.password = req.body.password;
+			}
 
-      const updatedUser = await user.save();
+			const updatedUser = await user.save();
 
-      res.json({
-        _id: updatedUser._id,
-        name: updatedUser.firstName + " " + updatedUser.lastName,
-        password: updatedUser.password,
-        email: updatedUser.email,
-        isAdmin: updatedUser.isAdmin,
-      });
-    } else {
-      res.status(404);
-      throw new Error("User not found");
-    }
-  } catch (error) {
-    res.status(500).json({ message: `${error.message}` });
-  }
+			res.json({
+				_id: updatedUser._id,
+				name: updatedUser.firstName + " " + updatedUser.lastName,
+				password: updatedUser.password,
+				email: updatedUser.email,
+				isAdmin: updatedUser.isAdmin,
+			});
+		} else {
+			res.status(404);
+			throw new Error("User not found");
+		}
+	} catch (error) {
+		res.status(500).json({ message: `${error.message}` });
+	}
 });
 
 // @desc  Delete User
 // route  DELETE/api/users/
 // access Private
 const deleteUserById = asyncHandler(async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByIdAndDelete(id, req.body);
-    if (!user) {
-      res.status(404).json("[User] not Found!");
-    } else {
-      const deletedUser = await User.findById(id);
-      return res.send(deletedUser);
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const { id } = req.params;
+		const user = await User.findByIdAndDelete(id, req.body);
+		if (!user) {
+			res.status(404).json("[User] not Found!");
+		} else {
+			const deletedUser = await User.findById(id);
+			return res.send(deletedUser);
+		}
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 });
 
 export {
-  authUser,
-  logOutUser,
-  getUsers,
-  getUserById,
-  createUser,
-  updateUserById,
-  deleteUserById,
+	authUser,
+	logOutUser,
+	getUsers,
+	getUserById,
+	createUser,
+	updateUserById,
+	deleteUserById,
 };
--- Web/src/Admin/pages/Dashboard/Dashboard.tsx
+++ Web/src/Admin/pages/Dashboard/Dashboard.tsx
@@ -9,67 +9,67 @@ import { useNavigate } from "react-router-dom";
 import ADMIN_ROUTES from "constants/adminRoutes";
 import { BiLogOut } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
+import AvatarSetter from "Admin/helpers/AvatarSetter";
 import "./Dashboard.scss";
 
 const Dashboard: React.FC = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: any) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const logoutHandler = () => {
-    localStorage.removeItem("user");
-    navigate(ADMIN_ROUTES.ADMIN);
-  };
+	const logoutHandler = () => {
+		localStorage.removeItem("user");
+		navigate(ADMIN_ROUTES.ADMIN);
+	};
 
-  // Local Storage Value Variables
-  const localStorageResponse = localStorage.getItem("user");
-  const response = JSON.parse(localStorageResponse ?? "");
-  const userName = response.name;
-  const userInitial = response.name.substring(0, 1);
+	// Local Storage Value Variables
+	const localStorageResponse = localStorage.getItem("user");
+	const response = JSON.parse(localStorageResponse ?? "");
+	const userName = response.name;
 
-  return (
-    <div className="dashboard-container">
-      <Grid container spacing={1}>
-        <Grid item sm={2}>
-          <div className="dashboard-sidebar">
-            <Sidebar />
-          </div>
-        </Grid>
-        <Grid item sm={10}>
-          <div className="dashboard-main-container">
-            <div className="header-panel-container">
-              <button onClick={handleClick}>
-                <Avatar sx={{ bgcolor: deepPurple[500] }}>{userInitial}</Avatar>
-              </button>
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                className="profile-menu"
-              >
-                <h2>{userName}</h2>
-                <MenuItem onClick={handleClose}>
-                  {" "}
-                  <CgProfile /> My account{" "}
-                </MenuItem>
-                <MenuItem onClick={logoutHandler}>
-                  <BiLogOut /> Log Out
-                </MenuItem>
-              </Menu>
-            </div>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
+	return (
+		<div className="dashboard-container">
+			<Grid container spacing={1}>
+				<Grid item sm={2}>
+					<div className="dashboard-sidebar">
+						<Sidebar />
+					</div>
+				</Grid>
+				<Grid item sm={10}>
+					<div className="dashboard-main-container">
+						<div className="header-panel-container">
+							<button onClick={handleClick}>
+								<AvatarSetter name={userName} />
+							</button>
+							<Menu
+								id="basic-menu"
+								anchorEl={anchorEl}
+								open={open}
+								onClose={handleClose}
+								className="profile-menu"
+							>
+								<h2>{userName}</h2>
+								<MenuItem onClick={handleClose}>
+									{" "}
+									<CgProfile /> My account{" "}
+								</MenuItem>
+								<MenuItem onClick={logoutHandler}>
+									<BiLogOut /> Log Out
+								</MenuItem>
+							</Menu>
+						</div>
+					</div>
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 
 export default Dashboard;
