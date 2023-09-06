0449ca132f574366fe82f42e4056cefbd25beace
Darryle Bacay <dmilesbacay@gmail.com>
2023-09-07 00:52:02 +0800

    recreate user table in dashboard

--- Web/src/Admin/pages/Dashboard/Dashboard.tsx
+++ Web/src/Admin/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import AdminWrapper from "Admin/layout/AdminWrapper/AdminWrapper";
+import { Grid } from "@mui/material";
+import UserTable from "./components/UserTable/UserTable";
+import "./Dashboard.scss";
 
 const Dashboard: React.FC = () => {
   return (
     <div>
       <AdminWrapper>
-        <h1>hello</h1>
+        <Grid container spacing={2} className="dashboard-container">
+          <Grid item md={7}>
+            <div className="user-table-container">
+              <UserTable />
+            </div>
+          </Grid>
+        </Grid>
       </AdminWrapper>
     </div>
   );
