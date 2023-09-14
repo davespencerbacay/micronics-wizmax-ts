0887a3ef4938e65967a2d049ac78875eecc24186
Darryle <dmilesbacay@gmail.com>
2023-09-15 02:05:06 +0800

    style changes

--- Web/src/Admin/pages/Dashboard/components/UserTable/UserTable.tsx
+++ Web/src/Admin/pages/Dashboard/components/UserTable/UserTable.tsx
@@ -5,149 +5,154 @@ import Spinner from "library/Spinner/Spinner";
 import { Users } from "Admin/api/agent";
 import { UsersData } from "Admin/models/userModel";
 import { toast } from "react-toastify";
-import "./UserTable.scss";
 import { Drawer, List } from "@mui/material";
 import EditUser from "Admin/pages/Users/components/EditUser";
+import "./UserTable.scss";
 
 type Anchor = "right";
 
 const UserTable: React.FC = () => {
-  const [users, setUsers] = useState<UsersData[] | undefined>();
-  const [loading, setLoading] = useState(false);
-  const [userId, setUserId] = useState<string>("");
+	const [users, setUsers] = useState<UsersData[] | undefined>();
+	const [loading, setLoading] = useState(false);
+	const [userId, setUserId] = useState<string>("");
 
-  // Fetch All Users Function
-  useEffect(() => {
-    setLoading(true);
-    const getUsers = async () => {
-      const response = await Users.getUsers();
-      setUsers(response);
-      setLoading(false);
-    };
+	// Fetch All Users Function
+	useEffect(() => {
+		setLoading(true);
+		const getUsers = async () => {
+			const response = await Users.getUsers();
+			setUsers(response);
+			setLoading(false);
+		};
 
-    getUsers();
-  }, []);
+		getUsers();
+	}, []);
 
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "fullName",
-      headerName: "Fullname",
-      width: 150,
-      editable: false,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 150,
-      editable: false,
-    },
-    {
-      field: "dateCreated",
-      headerName: "Date Created",
-      width: 800,
-      editable: false,
-    },
-    {
-      field: "actions",
-      headerName: "Actions",
-      width: 200,
-      renderCell: (params) => params.value,
-      headerAlign: "center",
-      align: "center",
-    },
-  ];
+	const columns: GridColDef[] = [
+		{ field: "id", headerName: "ID", width: 90, editable: true },
+		{
+			field: "fullName",
+			headerName: "Fullname",
+			width: 150,
+			editable: false,
+		},
+		{
+			field: "email",
+			headerName: "Email",
+			width: 150,
+			editable: false,
+		},
+		{
+			field: "dateCreated",
+			headerName: "Date Created",
+			width: 800,
+			editable: false,
+		},
+		{
+			field: "actions",
+			headerName: "Actions",
+			width: 200,
+			renderCell: (params) => params.value,
+			headerAlign: "center",
+			align: "center",
+		},
+	];
 
-  const deleteUserHandler = async (userId: string) => {
-    setLoading(true);
-    await Users.deleteUser(userId);
-    toast.success("User successfully Deleted");
-    setLoading(false);
+	const deleteUserHandler = async (userId: string) => {
+		setLoading(true);
+		await Users.deleteUser(userId);
+		toast.success("User successfully Deleted");
+		setLoading(false);
 
-    const filteredUsers = users?.filter((data) => data._id !== userId);
-    setUsers(filteredUsers);
-  };
+		const filteredUsers = users?.filter((data) => data._id !== userId);
+		setUsers(filteredUsers);
+	};
 
-  // Edit User Function
-  const [state, setState] = useState({
-    right: false,
-  });
+	// Edit User Function
+	const [state, setState] = useState({
+		right: false,
+	});
 
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean, id?: string | undefined) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
+	const toggleDrawer =
+		(anchor: Anchor, open: boolean, id?: string | undefined) =>
+		(event: React.KeyboardEvent | React.MouseEvent) => {
+			if (
+				event.type === "keydown" &&
+				((event as React.KeyboardEvent).key === "Tab" ||
+					(event as React.KeyboardEvent).key === "Shift")
+			) {
+				return;
+			}
 
-      setState({ ...state, [anchor]: open });
-      setUserId(id ?? "");
-    };
+			setState({ ...state, [anchor]: open });
+			setUserId(id ?? "");
+		};
 
-  const rows = users?.map((user) => {
-    return {
-      id: user._id,
-      fullName: user.firstName + " " + user.lastName,
-      email: user.email,
-      dateCreated: user.createdAt,
-      actions: (
-        <div className="grid-actions-btn">
-          <button
-            className="edit-btn"
-            onClick={toggleDrawer("right", true, user._id)}
-          >
-            Edit
-          </button>
+	const rows = users?.map((user) => {
+		const date = new Date(user.createdAt);
+		const formattedDate = date.toDateString();
 
-          <button
-            className="delete-btn"
-            onClick={() => deleteUserHandler(user._id)}
-          >
-            Delete
-          </button>
-        </div>
-      ),
-    };
-  });
+		const fullName = `${user.firstName} ${user.lastName}`;
 
-  if (loading) {
-    return <Spinner variant="fixed" />;
-  }
+		return {
+			id: user._id,
+			fullName: fullName.toUpperCase(),
+			email: user.email,
+			dateCreated: formattedDate,
+			actions: (
+				<div className="grid-actions-btn">
+					<button
+						className="edit-btn"
+						onClick={toggleDrawer("right", true, user._id)}
+					>
+						Edit
+					</button>
 
-  return (
-    <div>
-      <Box sx={{ height: 400, width: "100%" }}>
-        <DataGrid
-          rows={rows || []}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 20,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-          disableRowSelectionOnClick
-        />
-      </Box>
-      <Drawer
-        anchor="right"
-        open={state["right"]}
-        onClose={toggleDrawer("right", false)}
-      >
-        <Box sx={{ width: 400 }} role="presentation">
-          <List>
-            <EditUser id={userId} />
-          </List>
-        </Box>
-      </Drawer>
-    </div>
-  );
+					<button
+						className="delete-btn"
+						onClick={() => deleteUserHandler(user._id)}
+					>
+						Delete
+					</button>
+				</div>
+			),
+		};
+	});
+
+	if (loading) {
+		return <Spinner variant="fixed" />;
+	}
+
+	return (
+		<div>
+			<Box sx={{ height: 400, width: "100%" }}>
+				<DataGrid
+					rows={rows || []}
+					columns={columns}
+					initialState={{
+						pagination: {
+							paginationModel: {
+								pageSize: 20,
+							},
+						},
+					}}
+					pageSizeOptions={[5]}
+					disableRowSelectionOnClick
+				/>
+			</Box>
+			<Drawer
+				anchor="right"
+				open={state["right"]}
+				onClose={toggleDrawer("right", false)}
+			>
+				<Box sx={{ width: 400 }} role="presentation">
+					<List>
+						<EditUser id={userId} />
+					</List>
+				</Box>
+			</Drawer>
+		</div>
+	);
 };
 
 export default UserTable;
