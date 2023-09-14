ba8c3cf81bbbe9d256e1af410935dcb1aa5490ab
Darryle <dmilesbacay@gmail.com>
2023-09-15 02:48:29 +0800

    revise style

--- Web/src/Admin/pages/Dashboard/Dashboard.tsx
+++ Web/src/Admin/pages/Dashboard/Dashboard.tsx
@@ -5,19 +5,19 @@ import UserTable from "./components/UserTable/UserTable";
 import "./Dashboard.scss";
 
 const Dashboard: React.FC = () => {
-  return (
-    <div>
-      <AdminWrapper>
-        <Grid container spacing={2} className="dashboard-container">
-          <Grid item md={7}>
-            <div className="user-table-container">
-              <UserTable />
-            </div>
-          </Grid>
-        </Grid>
-      </AdminWrapper>
-    </div>
-  );
+	return (
+		<div>
+			<AdminWrapper>
+				<Grid container spacing={2} className="dashboard-container">
+					<Grid item md={7}>
+						<div className="user-table-container">
+							<UserTable />
+						</div>
+					</Grid>
+				</Grid>
+			</AdminWrapper>
+		</div>
+	);
 };
 
 export default Dashboard;
