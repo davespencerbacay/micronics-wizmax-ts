d610fc1c5cab0132517751033e1bf738fd22be67
Darryle <dmilesbacay@gmail.com>
2023-09-19 02:00:15 +0800

    create new user table for dashboard

--- Web/src/Admin/pages/Dashboard/Dashboard.tsx
+++ Web/src/Admin/pages/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import AdminWrapper from "Admin/layout/AdminWrapper/AdminWrapper";
 import { Grid } from "@mui/material";
 import UserTable from "./components/UserTable/UserTable";
 import "./Dashboard.scss";
+import DashBoardUser from "./components/UserTable/DashBoardUser";
 
 const Dashboard: React.FC = () => {
 	return (
@@ -11,7 +12,7 @@ const Dashboard: React.FC = () => {
 				<Grid container spacing={2} className="dashboard-container">
 					<Grid item md={7}>
 						<div className="user-table-container">
-							<UserTable />
+							<DashBoardUser />
 						</div>
 					</Grid>
 				</Grid>
