50169da476be19e4e714b8b75c09e1ec057fb4bc
darryle bacay <dmilesbacay@gmail.com>
2023-09-27 02:52:05 +0800

    remove slash on dashboard route

--- Web/src/constants/adminRoutes.tsx
+++ Web/src/constants/adminRoutes.tsx
@@ -1,9 +1,9 @@
 const ADMIN_ROUTES = {
-  ADMIN: "/admin",
-  DASHBOARD: "/dashboard/",
-  PROFILE: "/profile",
-  CALENDARS: "/calendars",
-  USERS: "/users",
+	ADMIN: "/admin",
+	DASHBOARD: "/dashboard",
+	PROFILE: "/profile",
+	CALENDARS: "/calendars",
+	USERS: "/users",
 };
 
 export default ADMIN_ROUTES;
