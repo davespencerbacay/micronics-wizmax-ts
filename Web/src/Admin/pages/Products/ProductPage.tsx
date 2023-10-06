e3fd23e2153a2d169b2e711131dab91077d87955
darryle bacay <dmilesbacay@gmail.com>
2023-10-06 23:30:45 +0800

    add loading on product page

--- Web/src/Admin/pages/Products/ProductPage.tsx
+++ Web/src/Admin/pages/Products/ProductPage.tsx
@@ -8,59 +8,65 @@ import { BiPlus } from "react-icons/bi";
 import AdminButton from "library/AdminButton/AdminButton";
 import ProductCard from "library/ProductCard/ProductCard";
 import "./ProductPage.scss";
+import Spinner from "library/Spinner/Spinner";
 
 const ProductPage: React.FC = () => {
-  const [products, setProducts] = useState<ProductsData[] | undefined>();
+	const [products, setProducts] = useState<ProductsData[] | undefined>();
+	const [loading, setLoading] = useState(false);
+	// Fetch All Products
+	useEffect(() => {
+		const getProducts = async () => {
+			setLoading(true);
+			const products = await Products.getAllProducts();
+			setProducts(products);
+			setLoading(false);
+		};
+		getProducts();
+	}, []);
 
-  // Fetch All Products
-  useEffect(() => {
-    const getProducts = async () => {
-      const products = await Products.getAllProducts();
-      setProducts(products);
-      console.log(products);
-    };
-    getProducts();
-  }, []);
+	const crumbs: CrumbTypes[] = [
+		{
+			title: "Wizmax Global",
+			url: ROUTE_PATH.DASHBOARD,
+			isActive: false,
+		},
+		{
+			title: "Products",
+			url: ROUTE_PATH.PRODUCTS,
+			isActive: true,
+		},
+	];
 
-  const crumbs: CrumbTypes[] = [
-    {
-      title: "Wizmax Global",
-      url: ROUTE_PATH.DASHBOARD,
-      isActive: false,
-    },
-    {
-      title: "Products",
-      url: ROUTE_PATH.PRODUCTS,
-      isActive: true,
-    },
-  ];
-
-  return (
-    <AdminWrapper breadcrumb={crumbs}>
-      <div className="product-page-container">
-        <div className="product-top-actions">
-          <div className="content-label">
-            <h3>Products</h3>
-            <p>List of all products.</p>
-          </div>
-          <AdminButton variant="default">
-            <BiPlus /> Add a Product
-          </AdminButton>
-        </div>
-        <div className="product-table">
-          {products?.map((product) => {
-            return (
-              <ProductCard
-                image={product.productImage}
-                name={product.productName}
-                description={product.productDescription}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </AdminWrapper>
-  );
+	return (
+		<AdminWrapper breadcrumb={crumbs}>
+			{loading ? (
+				<Spinner variant="fixed" />
+			) : (
+				<div className="product-page-container">
+					<div className="product-top-actions">
+						<div className="content-label">
+							<h3>Products</h3>
+							<p>List of all products.</p>
+						</div>
+						<AdminButton variant="default">
+							<BiPlus /> Add a Product
+						</AdminButton>
+					</div>
+					<div className="product-table">
+						{products?.map((product) => {
+							return (
+								<ProductCard
+									image={product.productImage}
+									name={product.productName}
+									description={product.productDescription}
+								/>
+							);
+						})}
+					</div>
+				</div>
+			)}
+		</AdminWrapper>
+	);
 };
 
 export default ProductPage;
