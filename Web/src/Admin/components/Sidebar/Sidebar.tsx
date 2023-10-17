ad4c4549c5f435672668099ad7b4f56c453c73e3
darryle bacay <dmilesbacay@gmail.com>
2023-10-17 20:59:57 +0800

    remove sidebar footer

--- Web/src/Admin/components/Sidebar/Sidebar.tsx
+++ Web/src/Admin/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
+	ProSidebar,
+	Menu,
+	MenuItem,
+	SidebarHeader,
+	SidebarFooter,
+	SidebarContent,
 } from "react-pro-sidebar";
 import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 import { BsGlobe } from "react-icons/bs";
@@ -15,58 +15,61 @@ import SidebarLinks from "./SidebarLinks";
 import { ROUTE_PATH } from "constants/routes";
 
 const Sidebar: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const [toggled, setToggled] = useState(false);
+	const [collapsed, setCollapsed] = useState(false);
+	const [toggled, setToggled] = useState(false);
 
-  return (
-    <ProSidebar
-      collapsed={collapsed}
-      toggled={toggled}
-      onToggle={() => setToggled(true)}
-      breakPoint="sm"
-    >
-      {/* Header */}
-      <SidebarHeader>
-        <Menu iconShape="circle">
-          {collapsed ? (
-            <MenuItem
-              icon={<FaAngleDoubleRight />}
-              onClick={() => setCollapsed((prevState) => !prevState)}
-            ></MenuItem>
-          ) : (
-            <MenuItem
-              suffix={<FaAngleDoubleLeft />}
-              onClick={() => setCollapsed((prevState) => !prevState)}
-            >
-              <div
-                style={{
-                  padding: "9px",
-                  textTransform: "uppercase",
-                  fontWeight: "bold",
-                  fontSize: 15,
-                  letterSpacing: "1px",
-                }}
-              >
-                Wizmax
-              </div>
-            </MenuItem>
-          )}
-        </Menu>
-      </SidebarHeader>
-      {/* Content */}
-      <SidebarContent>
-        <Menu iconShape="circle">
-          {SidebarLinks.map((item, index) => {
-            return (
-              <React.Fragment>
-                <MenuItem icon={item.icon} key={index}>
-                  {item.name}
-                  <NavLink to={item.linkTo} />
-                </MenuItem>
-              </React.Fragment>
-            );
-          })}
-          {/* <MenuItem icon={<RxDashboard />}>
+	const navigate = useNavigate();
+
+	return (
+		<ProSidebar
+			collapsed={collapsed}
+			toggled={toggled}
+			onToggle={() => setToggled(true)}
+			breakPoint="sm"
+		>
+			{/* Header */}
+			<SidebarHeader>
+				<Menu iconShape="circle">
+					{collapsed ? (
+						<MenuItem
+							icon={<FaAngleDoubleRight />}
+							onClick={() => setCollapsed((prevState) => !prevState)}
+						></MenuItem>
+					) : (
+						<MenuItem
+							suffix={<FaAngleDoubleLeft />}
+							onClick={() => setCollapsed((prevState) => !prevState)}
+						>
+							<div
+								style={{
+									padding: "9px",
+									textTransform: "uppercase",
+									fontWeight: "600",
+									fontSize: 15,
+									letterSpacing: "1px",
+								}}
+								onClick={() => navigate(ROUTE_PATH.INDEX)}
+							>
+								Wizmax
+							</div>
+						</MenuItem>
+					)}
+				</Menu>
+			</SidebarHeader>
+			{/* Content */}
+			<SidebarContent>
+				<Menu iconShape="circle">
+					{SidebarLinks.map((item, index) => {
+						return (
+							<React.Fragment>
+								<MenuItem icon={item.icon} key={index}>
+									{item.name}
+									<NavLink to={item.linkTo} />
+								</MenuItem>
+							</React.Fragment>
+						);
+					})}
+					{/* <MenuItem icon={<RxDashboard />}>
 						Dashboard
 						<NavLink to={ADMIN_ROUTES.DASHBOARD} />
 					</MenuItem>
@@ -75,13 +78,13 @@ const Sidebar: React.FC = () => {
 						<NavLink to={ADMIN_ROUTES.USERS} />
 					</MenuItem>
 					{/* <MenuItem icon={<FaGem />}>Components </MenuItem> */}
-          {/* <MenuItem icon={<FaClipboardList />}>
+					{/* <MenuItem icon={<FaClipboardList />}>
 						Products <Link to={ADMIN_ROUTES.PRODUCTS} />
 					</MenuItem>
 					<MenuItem icon={<FaQuestion />}>
 						FAQs <Link to={ADMIN_ROUTES.FAQs} />
 					</MenuItem> */}
-          {/* <SubMenu title={"With Suffix"} icon={<FaRegLaughWink />}>
+					{/* <SubMenu title={"With Suffix"} icon={<FaRegLaughWink />}>
 						<MenuItem>Submenu 1</MenuItem>
 						<MenuItem>Submenu 2</MenuItem>
 						<MenuItem>Submenu 3</MenuItem>
@@ -99,11 +102,11 @@ const Sidebar: React.FC = () => {
 							<MenuItem>Submenu 3.2 </MenuItem>
 						</SubMenu>
 					</SubMenu> */}
-          {/* Commented for future use, please discard for now. */}
-        </Menu>
-      </SidebarContent>
-      {/* Footer */}
-      <SidebarFooter style={{ textAlign: "center" }}>
+					{/* Commented for future use, please discard for now. */}
+				</Menu>
+			</SidebarContent>
+			{/* Footer */}
+			{/* <SidebarFooter style={{ textAlign: "center" }}>
         <div className="sidebar-btn-wrapper" style={{ padding: "16px" }}>
           <Link
             className="sidebar-btn"
@@ -122,9 +125,9 @@ const Sidebar: React.FC = () => {
             </span>
           </Link>
         </div>
-      </SidebarFooter>
-    </ProSidebar>
-  );
+      </SidebarFooter> */}
+		</ProSidebar>
+	);
 };
 
 export default Sidebar;
