88e65b83e369f4b0048c84bb232a43b40e3cf2b7
darryle bacay <dmilesbacay@gmail.com>
2023-10-17 19:39:15 +0800

    refactor code

--- Web/src/constants/AdminReactRoutes.tsx
+++ Web/src/constants/AdminReactRoutes.tsx
@@ -4,53 +4,53 @@ import Portal from "Admin/Portal";
 import GuardedWrapper from "Admin/layout/GuardedWrapper/GuardedWrapper";
 import UserPage from "Admin/pages/Users/UserPage";
 import ProductPage from "Admin/pages/Products/ProductPage";
-import CreateProduct from "Admin/pages/Products/components/CreateProduct";
+import ProductForm from "Admin/pages/Products/components/ProductForm";
 
 const ADMIN_REACT_ROUTES = [
-  {
-    PATH: ROUTE_PATH.ADMIN,
-    ELEMENT: <Portal />,
-  },
-  {
-    PATH: ROUTE_PATH.DASHBOARD,
-    ELEMENT: (
-      <GuardedWrapper>
-        <Dashboard />
-      </GuardedWrapper>
-    ),
-  },
-  {
-    PATH: ROUTE_PATH.USERS,
-    ELEMENT: (
-      <GuardedWrapper>
-        <UserPage />
-      </GuardedWrapper>
-    ),
-  },
-  {
-    PATH: ROUTE_PATH.PRODUCTS,
-    ELEMENT: (
-      <GuardedWrapper>
-        <ProductPage />
-      </GuardedWrapper>
-    ),
-  },
-  {
-    PATH: ROUTE_PATH.CREATEPRODUCT,
-    ELEMENT: (
-      <GuardedWrapper>
-        <CreateProduct />
-      </GuardedWrapper>
-    ),
-  },
-  {
-    PATH: ROUTE_PATH.UPDATEPRODUCT,
-    ELEMENT: (
-      <GuardedWrapper>
-        <CreateProduct />
-      </GuardedWrapper>
-    ),
-  },
+	{
+		PATH: ROUTE_PATH.ADMIN,
+		ELEMENT: <Portal />,
+	},
+	{
+		PATH: ROUTE_PATH.DASHBOARD,
+		ELEMENT: (
+			<GuardedWrapper>
+				<Dashboard />
+			</GuardedWrapper>
+		),
+	},
+	{
+		PATH: ROUTE_PATH.USERS,
+		ELEMENT: (
+			<GuardedWrapper>
+				<UserPage />
+			</GuardedWrapper>
+		),
+	},
+	{
+		PATH: ROUTE_PATH.PRODUCTS,
+		ELEMENT: (
+			<GuardedWrapper>
+				<ProductPage />
+			</GuardedWrapper>
+		),
+	},
+	{
+		PATH: ROUTE_PATH.CREATEPRODUCT,
+		ELEMENT: (
+			<GuardedWrapper>
+				<ProductForm />
+			</GuardedWrapper>
+		),
+	},
+	{
+		PATH: ROUTE_PATH.UPDATEPRODUCT,
+		ELEMENT: (
+			<GuardedWrapper>
+				<ProductForm />
+			</GuardedWrapper>
+		),
+	},
 ];
 
 export default ADMIN_REACT_ROUTES;
